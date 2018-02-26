134893001b193e6253c200438821cfa75e576ce2
ITheCon <cksmith@net-ex.no>
2018-02-26 17:47:28 +0000

    added Player scripts

---
+++ Main Unity Project/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class CameraMovement : MonoBehaviour {
+    // Use this for initialization
+    public GameObject target;
+    void MoveCamera()
+    {
+        Vector3 playerPos = target.transform.position;
+        float x = playerPos.x - transform.position.x;
+        float y = playerPos.y - transform.position.y;
+        Vector3 add = new Vector3(x, y, 0);
+        transform.position += add;
+    }
+
+    void ConfigureOrthograhic()
+    {
+        
+        float x = Screen.width;
+        float y = Screen.height;
+        float Size = 2 * (x / (((x / y) * 2) * 64));
+        Camera.main.orthographicSize = Size;
+    }   
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        MoveCamera();
+        ConfigureOrthograhic();
+
+
+
+    }
+
+}
---
+++ Main Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public float health = 5.0f;
+    private Rigidbody2D phys;
+    private bool flipped = false;
+    Vector2 pos;
+    Vector3 intitialPos = new Vector3(0, 0, 0);
+    Vector3 movedPos = new Vector3(0, 0, 0);
+
+    public void takeDamage(float damage)
+    {
+        Debug.Log("damage taken!");
+        health = health - damage;
+    }
+    void flip()
+    {
+        flipped = !flipped;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+    void keyboardMovement()
+    {
+        Vector3 horizontal = (transform.right * (Input.GetAxis("Horizontal") / 2.0f) * speed * Time.fixedDeltaTime) * 64 * 2;
+        Vector3 vertical = (transform.up * (Input.GetAxis("Vertical") / 2.0f) * speed * Time.fixedDeltaTime) * 64 * 2;
+        if (horizontal.x > 0 && flipped)
+            flip();
+        else if (horizontal.x < 0 && !flipped)
+            flip();
+        transform.position += horizontal;
+        transform.position += vertical;
+    }
+    void tabletMovement()
+    {
+        Touch[] myTouches = Input.touches;
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch touch = myTouches[i];
+            if (touch.position.x < Screen.width / 2)
+            {
+                if (touch.phase == TouchPhase.Began)
+                    intitialPos = touch.position;
+                else if (touch.phase == TouchPhase.Moved)
+                    movedPos = touch.position;
+                float xDifference = movedPos.x - intitialPos.x;
+                float yDifference = movedPos.y - intitialPos.y;
+                float actualDifference = Mathf.Sqrt((xDifference * xDifference) + (yDifference * yDifference));
+
+                float ratioDifference = 50 / actualDifference;
+                float mX = xDifference * ratioDifference;
+                float mY = yDifference * ratioDifference;
+                Vector3 horizontal = (transform.right * (mX / 200.0f) * speed * Time.fixedDeltaTime) * 64 * 2;
+                Vector3 vertical = (transform.up * (mY / 200.0f) * speed * Time.fixedDeltaTime) * 64 * 2;
+                if (horizontal.x > 0 && flipped)
+                    flip();
+                else if (horizontal.x < 0 && !flipped)
+                    flip();
+                transform.position += horizontal;
+                transform.position += vertical;
+            }
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        
+        phys = GetComponent<Rigidbody2D>();
+        pos = transform.position;
+
+        Camera.main.GetComponent<CameraMovement>().target = gameObject.transform.GetChild(0).gameObject;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            
+        }
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+        keyboardMovement();
+        //tabletMovement();
+    }
+
+    
+}
